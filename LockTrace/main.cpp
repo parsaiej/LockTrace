c8c47c01fbb4fccd30a840c5b67a5a0c4fb2b8be
Parsaie <john.parsaie@mymail.champlain.edu>
2017-04-20 15:18:06 -0400

    Vec3 work

--- LockTrace/main.cpp
+++ LockTrace/main.cpp
@@ -31,9 +31,28 @@ public:
 	inline float sprLength() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];	   }
 	inline void normalize();
 		
-
 	float e[3];
+
+
 };
+
+inline std::istream& operator >> (std::istream &is, vec3 &t) {
+	is >> t.e[0] >> t.e[1] >> t.e[2];
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+	os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+}
+
+inline void vec3::normalize() {
+	float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+	e[0] *= k; e[1] *= k; e[2] *= k;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
 
 int main() {
 
